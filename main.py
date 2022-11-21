60b5a5bf23212c1fb0a6bcffa3f6c3af1a7904e4
singurty <singurty@gmail.com>
2022-11-21 11:06:21 +0545

    figure bit

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ bin_data = bin_file.read()
 magic = bytes([0x7f, 0x45, 0x4c, 0x46])
 if bin_data[:4] != magic:
     print("invalid file: not an elf binary")
-    print(bin_data[:4])
-    print(type(bin_data))
+    exit(1)
+
+bit = 32
+if bin_data[4] == 1:
+    bit =32
+elif bin_data[4] == 2:
+    bit = 64
+else:
+    print("could not determine bit architecture")
+    exit(1)
+
+print("the program is {}-bit".format(bit))
