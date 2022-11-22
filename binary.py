628d3df5354b2b0dea8500bc307c50879019af7a
singurty <singurty@gmail.com>
2022-11-22 17:30:56 +0545

    read some more cool stuff
    change the way we keep track of endianness to avoid hardcoding it to the
    byteorder parameter of int.from_bytes()

--- binary.py
+++ binary.py
@@ -26,9 +26,12 @@ class Binary:
             print("could not determine bit architecture")
             exit(1)
 
-        # 1 is little, 2 is big
-        self.endian = bin_data[5]
-        if not self.endian == 1 or self.endian == 2:
+        # figure byteorder
+        if bin_data[5] == 1:
+            self.endian = "little"
+        elif bin_data[5] == 2:
+            self.endian = "big"
+        else:
             print("invalid endianness")
             exit(1)
 
@@ -80,7 +83,7 @@ class Binary:
         # there is no EI_ABIVERSION so treat bin_data[8] to bin_data[16] as EI_PAD
         # figure type of elf
         # e_type is two bytes
-        type_bytes = int.from_bytes(bin_data[16:18], byteorder="little")
+        type_bytes = int.from_bytes(bin_data[16:18], byteorder=self.endian)
         if type_bytes == 0x00:
             self.type = Type.NONE
         elif type_bytes == 0x01:
@@ -104,14 +107,14 @@ class Binary:
             exit(1)
 
         # figure e_machine. again two bytes.
-        machine_bytes = int.from_bytes(bin_data[18:20], byteorder="little")
+        machine_bytes = int.from_bytes(bin_data[18:20], byteorder=self.endian)
         if not machine_bytes in machine:
             print("invalid machine header: {}".format(hex(machine_bytes)))
         else:
             self.machine = machine_bytes
 
         # again version. bigger this time
-        version_bytes = int.from_bytes(bin_data[20:24], byteorder="little")
+        version_bytes = int.from_bytes(bin_data[20:24], byteorder=self.endian)
         if not version_bytes == 1:
             print("invalid elf version: {}".format(version_bytes))
             exit(1)
@@ -121,16 +124,55 @@ class Binary:
 
         # entry is 8 bytes for 64-bit and 4 bytes for 32-bit. fuck 32-bit for now.
         # stick to goal
-        self.entry_point = int.from_bytes(bin_data[i:i+8], byteorder="little")
+        self.entry = int.from_bytes(bin_data[i:i+8], byteorder=self.endian)
         i += 8
+
+        # figure program header table pointer. size same as entry
+        self.program_header = int.from_bytes(bin_data[i:i+8], byteorder=self.endian)
+        i += 8
+
+        # figure section header table poniter. size same as entry
+        self.section_header = int.from_bytes(bin_data[i:i+8], byteorder=self.endian)
+        i += 8
+
+        # skip e_flags which is 4 bytes
+        i += 4
+
+        # figure header size
+        self.header_size = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
+
+        # program header entry size
+        self.ph_size = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
+
+        # number of entries in the program header table
+        self.ph_num = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
+
+        # size of a entry in section header table
+        self.sh_size = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
+
+        # number of entries in the section header table
+        self.sh_num = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
+
+        # index of the section header table entry that contains the section names
+        self.sh_nameidx = int.from_bytes(bin_data[i:i+2], byteorder=self.endian)
+        i += 2
 
     def print_details(self):
         print("the binary is {}-bit".format(self.bit))
-        if self.endian == 1:
-            print("the binary is little endian")
-        else:
-            print("the binary is big endian")
+        print("the binary is {} endian".format(self.endian))
         print("the binary is for {}".format(OS(self.OS)))
         print("the elf type is {}".format(Type(self.type)))
         print("the machine this elf is for: {}".format(machine[self.machine]))
-        print("the entry point: {}".format(hex(self.entry_point)))
+        print("entry pointer: {}".format(hex(self.entry)))
+        print("program header pointer: {}".format(hex(self.program_header)))
+        print("elf header size: {}".format(self.header_size))
+        print("program header table entry size: {}".format(self.ph_size))
+        print("number of entries in the program header table: {}".format(self.ph_num))
+        print("section header table entry size: {}".format(self.sh_size))
+        print("number of entries in the section header table: {}".format(self.sh_num))
+        print("index of the section header entry that contains section names: {}".format(self.sh_nameidx))
